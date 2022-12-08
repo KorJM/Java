f4d4fc9df1094734a6c708824196df3405e05810
MINJO KIM <114234223+KorJM@users.noreply.github.com>
2022-12-08 18:09:08 +0900

    Add files via upload

---
+++ Swing/CoinEatingGameDriver.java
@@ -0,0 +1,11 @@
+package SwingProject;
+
+public class CoinEatingGameDriver {
+
+	public static void main(String[] args) {
+		CoinEatingGame coinEatingGame = new CoinEatingGame();
+		coinEatingGame.go();
+
+	}
+
+}
